ed78bc0f269b078c3e9fa4eb79b1b6e6c85abc76
JacobTuftsPaypoint <jacobtufts@paypoint.com>
2022-10-18 11:05:11 +0100

    env fix

--- api/GetAllEvents/index.js
+++ api/GetAllEvents/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     const cosmos = require('@azure/cosmos');
-    const endpoint = process.env.CosmosEndpoint;
-    const key = process.env.CosmosKey;
+    const endpoint = process.env["CosmosEndpoint"];
+    const key = process.env["CosmosKey"];
     const { CosmosClient } = cosmos;
 
     const client = new CosmosClient({ endpoint, key });
