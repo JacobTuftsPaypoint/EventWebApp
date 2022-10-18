174bd983ffc50121e081a829f2a18ee322fa9053
JacobTuftsPaypoint <jacobtufts@paypoint.com>
2022-10-18 10:51:30 +0100

    env fix

--- api/GetAllEvents/index.js
+++ api/GetAllEvents/index.js
@@ -1,13 +1,13 @@
-const cosmos = require('@azure/cosmos');
-const endpoint = process.env.CosmosEndpoint;
-const key = process.env.CosmosKey;
-const { CosmosClient } = cosmos;
-
-const client = new CosmosClient({ endpoint, key });
-const container = client.database("EventWebApp").container("Events");
-
-
 module.exports = async function (context, req) {
+    const cosmos = require('@azure/cosmos');
+    const endpoint = process.env.CosmosEndpoint;
+    const key = process.env.CosmosKey;
+    const { CosmosClient } = cosmos;
+
+    const client = new CosmosClient({ endpoint, key });
+    const container = client.database("EventWebApp").container("Events");
+
+
     context.log("Get Events called")
     
     const resource = await (await container.items.readAll().fetchAll()).resources
